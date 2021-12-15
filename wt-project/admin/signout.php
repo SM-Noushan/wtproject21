74c7aa2744a5804050bff4c811dd97d3dbba3b92
SM-Noushan <87843065+SM-Noushan@users.noreply.github.com>
2021-12-15 09:51:34 +0600

    Delete wt-project directory

--- wt-project/admin/signout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(session_status()>=0){
-        session_start();
-        session_unset();
-        session_destroy();
-        echo "Redirecting to Homepage";
-    }
-    header("refresh: 0.5, url = login.php");
-?>
